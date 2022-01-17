e657284fc781ec8a98698d0b6fc665b94f489627
Dhanfinix <66616896+Dhanfinix@users.noreply.github.com>
2022-01-17 13:55:38 +0700

    Belajar add firebase remote config, such as big a feature to make a app more mobile PR : multiple links for one webview, untuk hemat activity

--- app/src/main/java/com/dhandev/eepa/ui/home/HomeFragment.java
+++ app/src/main/java/com/dhandev/eepa/ui/home/HomeFragment.java
@@ -9,6 +9,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -23,25 +24,79 @@ import androidx.lifecycle.ViewModelProvider;
 import com.dhandev.eepa.Materi;
 import com.dhandev.eepa.R;
 import com.dhandev.eepa.databinding.FragmentHomeBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig;
+import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings;
+import com.squareup.picasso.Picasso;
 
 import org.imaginativeworld.whynotimagecarousel.CarouselItem;
 import org.imaginativeworld.whynotimagecarousel.ImageCarousel;
 import org.imaginativeworld.whynotimagecarousel.OnItemClickListener;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
+import java.util.Map;
 
 public class HomeFragment extends Fragment{
 
     private HomeViewModel homeViewModel;
     private FragmentHomeBinding binding;
-    ImageCarousel carousel;
+    private ImageCarousel carousel;
+    private Button test, update;
+    private ImageView imageView;
+
+    FirebaseRemoteConfig firebaseRemoteConfig;
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         homeViewModel = new ViewModelProvider(this).get(HomeViewModel.class);
         View view = inflater.inflate(R.layout.fragment_home, container, false); //imageview helper
         carousel = view.findViewById(R.id.carousel);
+        test    = view.findViewById(R.id.test);
+        update  = view.findViewById(R.id.update);
+        imageView  =view.findViewById(R.id.gambar);
+
+        //init
+        firebaseRemoteConfig = FirebaseRemoteConfig.getInstance();
+        FirebaseRemoteConfigSettings configSettings = new FirebaseRemoteConfigSettings.Builder().build();
+        firebaseRemoteConfig.setConfigSettingsAsync(configSettings);
+
+        //init default value for firebase remote config
+        Map<String, Object> defaultData = new HashMap<>();
+        defaultData.put("test","version 1.0");
+        defaultData.put("update", false);
+        defaultData.put("image_link","https://images.pexels.com/photos/2916826/pexels-photo-2916826.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940");
+        firebaseRemoteConfig.setDefaultsAsync(defaultData);
+
+        //load image
+        Picasso.get().load("https://images.pexels.com/photos/2916826/pexels-photo-2916826.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940")
+                .into(imageView);
+
+        //event pengambilan data baru dari firebase console
+        update.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                firebaseRemoteConfig.fetch(0) //time to live of cache, if 0 it will refresh without cache
+                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if(task.isSuccessful())
+                            {
+                                firebaseRemoteConfig.activate();
+                                test.setText(firebaseRemoteConfig.getString("test"));
+                                test.setEnabled(firebaseRemoteConfig.getBoolean("update"));
+                                Picasso.get().load(firebaseRemoteConfig.getString("image_link")).into(imageView);
+                            }
+                            else {
+                                Toast.makeText(getActivity(),""+task.getException().getMessage(),Toast.LENGTH_LONG);
+                            }
+                        }
+                    });
+            }
+        });
+
         //gambar pada carousel
         List<CarouselItem> listCar=new ArrayList<>();
 
